5f6200438ee5d2a1aec299293affce8cd55c5604
Pawel Stefaniak <x15023541@student.ncirl.ie>
2016-04-05 23:48:19 +0100

    init

---
+++ WebDevelopment/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebDevelopment.Startup))]
+namespace WebDevelopment
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
